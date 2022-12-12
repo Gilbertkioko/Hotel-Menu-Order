bf89c58c2fe36bcc096d1602467c6db84a927c86
Gilbertkioko <98327613+Gilbertkioko@users.noreply.github.com>
2022-12-12 14:25:35 +0300

    Add files via upload

---
+++ HOTEL MENU ORDER/Form1.Designer.vb
@@ -0,0 +1,286 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.btnGet = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnSave = New System.Windows.Forms.Button()
+        Me.btnTotal = New System.Windows.Forms.Button()
+        Me.txtTotal = New System.Windows.Forms.TextBox()
+        Me.txtQuantity = New System.Windows.Forms.TextBox()
+        Me.txtUnitPrice = New System.Windows.Forms.TextBox()
+        Me.cmbMenu = New System.Windows.Forms.ComboBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.btnPrint = New System.Windows.Forms.Button()
+        Me.dgvList = New System.Windows.Forms.DataGridView()
+        Me.ITEM = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.QUANTITY = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.TOTAL = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Panel1.SuspendLayout()
+        Me.Panel2.SuspendLayout()
+        CType(Me.dgvList, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.SystemColors.ControlLight
+        Me.Panel1.BackgroundImage = Global.HOTEL_MENU_ORDER.My.Resources.Resources.KWA_MATHE
+        Me.Panel1.Controls.Add(Me.btnGet)
+        Me.Panel1.Controls.Add(Me.btnClear)
+        Me.Panel1.Controls.Add(Me.btnSave)
+        Me.Panel1.Controls.Add(Me.btnTotal)
+        Me.Panel1.Controls.Add(Me.txtTotal)
+        Me.Panel1.Controls.Add(Me.txtQuantity)
+        Me.Panel1.Controls.Add(Me.txtUnitPrice)
+        Me.Panel1.Controls.Add(Me.cmbMenu)
+        Me.Panel1.Controls.Add(Me.Label5)
+        Me.Panel1.Controls.Add(Me.Label4)
+        Me.Panel1.Controls.Add(Me.Label3)
+        Me.Panel1.Controls.Add(Me.Label2)
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Location = New System.Drawing.Point(12, 12)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(546, 626)
+        Me.Panel1.TabIndex = 0
+        '
+        'btnGet
+        '
+        Me.btnGet.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btnGet.Location = New System.Drawing.Point(431, 291)
+        Me.btnGet.Name = "btnGet"
+        Me.btnGet.Size = New System.Drawing.Size(112, 34)
+        Me.btnGet.TabIndex = 14
+        Me.btnGet.Text = "GET"
+        Me.btnGet.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btnClear.Location = New System.Drawing.Point(431, 561)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(112, 34)
+        Me.btnClear.TabIndex = 13
+        Me.btnClear.Text = "CLEAR"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnSave
+        '
+        Me.btnSave.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btnSave.Location = New System.Drawing.Point(290, 561)
+        Me.btnSave.Name = "btnSave"
+        Me.btnSave.Size = New System.Drawing.Size(112, 34)
+        Me.btnSave.TabIndex = 12
+        Me.btnSave.Text = "SAVE"
+        Me.btnSave.UseVisualStyleBackColor = True
+        '
+        'btnTotal
+        '
+        Me.btnTotal.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btnTotal.Location = New System.Drawing.Point(431, 435)
+        Me.btnTotal.Name = "btnTotal"
+        Me.btnTotal.Size = New System.Drawing.Size(112, 34)
+        Me.btnTotal.TabIndex = 11
+        Me.btnTotal.Text = "TOTAL"
+        Me.btnTotal.UseVisualStyleBackColor = True
+        '
+        'txtTotal
+        '
+        Me.txtTotal.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.txtTotal.Location = New System.Drawing.Point(192, 505)
+        Me.txtTotal.Name = "txtTotal"
+        Me.txtTotal.Size = New System.Drawing.Size(97, 31)
+        Me.txtTotal.TabIndex = 9
+        '
+        'txtQuantity
+        '
+        Me.txtQuantity.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.txtQuantity.Location = New System.Drawing.Point(192, 356)
+        Me.txtQuantity.Name = "txtQuantity"
+        Me.txtQuantity.Size = New System.Drawing.Size(97, 31)
+        Me.txtQuantity.TabIndex = 8
+        '
+        'txtUnitPrice
+        '
+        Me.txtUnitPrice.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.txtUnitPrice.Location = New System.Drawing.Point(192, 297)
+        Me.txtUnitPrice.Name = "txtUnitPrice"
+        Me.txtUnitPrice.ReadOnly = True
+        Me.txtUnitPrice.Size = New System.Drawing.Size(97, 31)
+        Me.txtUnitPrice.TabIndex = 7
+        '
+        'cmbMenu
+        '
+        Me.cmbMenu.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.cmbMenu.FormattingEnabled = True
+        Me.cmbMenu.Items.AddRange(New Object() {"UGALI ", "RICE", "CHAPATI", "BEANS ", "BEEF", "1/4 CHICKEN", "300ml SODA", "SAUSAGE", "SMOKIE"})
+        Me.cmbMenu.Location = New System.Drawing.Point(192, 77)
+        Me.cmbMenu.Name = "cmbMenu"
+        Me.cmbMenu.Size = New System.Drawing.Size(311, 33)
+        Me.cmbMenu.TabIndex = 6
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label5.Location = New System.Drawing.Point(13, 508)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(77, 25)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "TOTAL :"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label4.Location = New System.Drawing.Point(13, 359)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(115, 25)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "QUANTITY :"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label3.Location = New System.Drawing.Point(13, 300)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(121, 25)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "UNIT PRICE :"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label2.Location = New System.Drawing.Point(13, 80)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(132, 25)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "SELECT ITEM :"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label1.Location = New System.Drawing.Point(103, 12)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(295, 25)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "PLACE CUSTOMER ORDERS HERE"
+        '
+        'Panel2
+        '
+        Me.Panel2.BackColor = System.Drawing.SystemColors.ControlDarkDark
+        Me.Panel2.Controls.Add(Me.btnPrint)
+        Me.Panel2.Controls.Add(Me.dgvList)
+        Me.Panel2.Location = New System.Drawing.Point(564, 12)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(521, 626)
+        Me.Panel2.TabIndex = 1
+        '
+        'btnPrint
+        '
+        Me.btnPrint.Font = New System.Drawing.Font("Segoe UI Black", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btnPrint.Location = New System.Drawing.Point(177, 391)
+        Me.btnPrint.Name = "btnPrint"
+        Me.btnPrint.Size = New System.Drawing.Size(158, 78)
+        Me.btnPrint.TabIndex = 1
+        Me.btnPrint.Text = "PRINT"
+        Me.btnPrint.UseVisualStyleBackColor = True
+        '
+        'dgvList
+        '
+        Me.dgvList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvList.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.ITEM, Me.QUANTITY, Me.TOTAL})
+        Me.dgvList.Location = New System.Drawing.Point(3, 12)
+        Me.dgvList.Name = "dgvList"
+        Me.dgvList.RowHeadersWidth = 62
+        Me.dgvList.RowTemplate.Height = 33
+        Me.dgvList.Size = New System.Drawing.Size(515, 225)
+        Me.dgvList.TabIndex = 0
+        '
+        'ITEM
+        '
+        Me.ITEM.HeaderText = "ITEM"
+        Me.ITEM.MinimumWidth = 8
+        Me.ITEM.Name = "ITEM"
+        Me.ITEM.Width = 150
+        '
+        'QUANTITY
+        '
+        Me.QUANTITY.HeaderText = "QUANTITY"
+        Me.QUANTITY.MinimumWidth = 8
+        Me.QUANTITY.Name = "QUANTITY"
+        Me.QUANTITY.Width = 150
+        '
+        'TOTAL
+        '
+        Me.TOTAL.HeaderText = "TOTAL"
+        Me.TOTAL.MinimumWidth = 8
+        Me.TOTAL.Name = "TOTAL"
+        Me.TOTAL.Width = 150
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(10.0!, 25.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1109, 647)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.Panel1)
+        Me.Name = "Form1"
+        Me.Text = "KWA MATHE PLACE"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.Panel2.ResumeLayout(False)
+        CType(Me.dgvList, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnSave As Button
+    Friend WithEvents btnTotal As Button
+    Friend WithEvents txtTotal As TextBox
+    Friend WithEvents txtQuantity As TextBox
+    Friend WithEvents txtUnitPrice As TextBox
+    Friend WithEvents cmbMenu As ComboBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents btnPrint As Button
+    Friend WithEvents dgvList As DataGridView
+    Friend WithEvents ITEM As DataGridViewTextBoxColumn
+    Friend WithEvents QUANTITY As DataGridViewTextBoxColumn
+    Friend WithEvents TOTAL As DataGridViewTextBoxColumn
+    Friend WithEvents btnGet As Button
+End Class
---
+++ HOTEL MENU ORDER/Form1.vb
@@ -0,0 +1,158 @@
+﻿Imports System.Drawing.Printing
+
+Public Class Form1
+    Dim WithEvents PD As New PrintDocument
+    Dim PPD As New PrintPreviewDialog
+    Dim LongPaper As Integer
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        cmbMenu.Text = " "
+        txtQuantity.Text = " "
+        txtTotal.Text = " "
+        txtUnitPrice.Text = " "
+    End Sub
+
+    Private Sub Panel2_Paint(sender As Object, e As PaintEventArgs) Handles Panel2.Paint
+
+    End Sub
+
+    Private Sub btnGet_Click(sender As Object, e As EventArgs) Handles btnGet.Click
+        Dim items() As Integer = {20, 30, 20, 50, 100, 150, 50, 50, 35} 'DECLARES FOOD PRICES
+
+        If cmbMenu.SelectedIndex = 0 Then 'IF UGALI IS SELECTED
+
+            txtUnitPrice.Text = Val(items(0))
+
+        ElseIf cmbMenu.SelectedIndex = 1 Then 'IF RICE IS SELECTED
+            txtUnitPrice.Text = Val(items(1))
+
+        ElseIf cmbMenu.SelectedIndex = 2 Then 'IF CHAPATI IS SELECTED
+            txtUnitPrice.Text = Val(items(2))
+
+        ElseIf cmbMenu.SelectedIndex = 3 Then 'IF BEANS IS SELECTED
+            txtUnitPrice.Text = Val(items(3))
+
+        ElseIf cmbMenu.SelectedIndex = 4 Then 'IF BEEF IS SELECTED
+            txtUnitPrice.Text = Val(items(4))
+
+        ElseIf cmbMenu.SelectedIndex = 5 Then 'IF KUKU IS SELECTED
+            txtUnitPrice.Text = Val(items(5))
+
+        ElseIf cmbMenu.SelectedIndex = 6 Then 'IF SODA IS SELECTED
+            txtUnitPrice.Text = Val(items(6))
+
+        ElseIf cmbMenu.SelectedIndex = 7 Then 'IF SAUSAGES IS SELECTED
+            txtUnitPrice.Text = Val(items(7))
+
+        ElseIf cmbMenu.SelectedIndex = 8 Then 'IF SMOKIE IS SELECTED
+            txtUnitPrice.Text = Val(items(8))
+
+
+
+        End If
+
+
+    End Sub
+
+    Private Sub btnTotal_Click(sender As Object, e As EventArgs) Handles btnTotal.Click
+        Dim UnitPrice, Quantity, Total As Integer
+        UnitPrice = txtUnitPrice.Text
+        Quantity = txtQuantity.Text
+        Total = UnitPrice * Quantity
+        txtTotal.Text = Val(Total)
+    End Sub
+
+    Private Sub btnSave_Click(sender As Object, e As EventArgs) Handles btnSave.Click
+        dgvList.Rows.Add(cmbMenu.Text, txtQuantity.Text, txtTotal.Text)
+
+    End Sub
+
+    Private Sub PrintDocument1_PrintPage(sender As Object, e As Printing.PrintPageEventArgs)
+
+    End Sub
+
+    Private Sub PD_BeginPrint(sender As Object, e As Printing.PrintEventArgs) Handles PD.BeginPrint
+        Dim PageSetup As New PageSettings
+        PageSetup.PaperSize = New PaperSize("custom", 250, 500) 'Determines the size of the receipt
+        PD.DefaultPageSettings = PageSetup
+
+    End Sub
+    Private Sub PD_PrintPage(sender As Object, e As PrintPageEventArgs) Handles PD.PrintPage
+        'DECLARING DIFFERENT FONTS FOR DIFFERENT THINGS IN THE RECEIPT AS VARIABLES, TO BE 
+        'CALLED LATER ON
+        Dim Font8 As New Font("Calibri", 8, FontStyle.Regular)
+        Dim Font8Italic As New Font("Comic Sans MS", 8, FontStyle.Italic)
+        Dim Font10Bold As New Font("Calibri", 10, FontStyle.Bold)
+        Dim Font14 As New Font("Calibri", 14, FontStyle.Bold)
+
+        'DECLARING MARGINS
+        Dim LeftMargin As Integer = PD.DefaultPageSettings.Margins.Left
+        Dim CentreMargin As Integer = PD.DefaultPageSettings.PaperSize.Width / 2
+        Dim RightMargin As Integer = PD.DefaultPageSettings.PaperSize.Width
+
+
+        'FONT ALIGNMENT 
+        Dim Right As New StringFormat
+        Dim centre As New StringFormat
+        Right.Alignment = StringAlignment.Far
+        centre.Alignment = StringAlignment.Center
+
+
+        Dim Line As String
+        Line = "====================================================="
+
+        e.Graphics.DrawString("KWA MATHE PLACE", Font14, Brushes.Black, CentreMargin, 5, centre)
+        e.Graphics.DrawString("GUARANTEED QUALITY AND QUANTITY", Font8Italic, Brushes.Black, CentreMargin, 30, centre)
+
+        e.Graphics.DrawString(Line, Font8, Brushes.Black, 0, 50)
+
+        e.Graphics.DrawString("QTY", Font10Bold, Brushes.Black, 0, 75)
+        e.Graphics.DrawString("ITEM", Font10Bold, Brushes.Black, CentreMargin, 75, centre)
+        e.Graphics.DrawString("TOTAL", Font10Bold, Brushes.Black, RightMargin, 75, Right)
+
+
+
+        'ACTUAL ITEMS
+        Dim Height As Integer
+        Dim i As Long
+        dgvList.AllowUserToAddRows = False
+        For row As Integer = 0 To dgvList.RowCount - 1
+            Height += 15
+            'NEXT LINE PRINTS QUANTITY(S) TO RECEIPT
+            e.Graphics.DrawString(dgvList.Rows(row).Cells(1).Value.ToString, Font8, Brushes.Black, 10, 100 + Height)
+            'GETS ITEM
+            e.Graphics.DrawString(dgvList.Rows(row).Cells(0).Value.ToString, Font8, Brushes.Black, CentreMargin, 100 + Height, centre)
+            'GETS TOTAL
+            e.Graphics.DrawString(dgvList.Rows(row).Cells(2).Value.ToString, Font8, Brushes.Black, RightMargin, 100 + Height, Right)
+
+        Next
+        'LINE AFTER LIST OF ITEMS
+        Dim Height2 As Integer
+        Height2 = 110 + Height
+
+        Sumprice() 'CALL THE SUB SUMPRICE
+
+        e.Graphics.DrawString(Line, Font8, Brushes.Black, 0, Height2)
+        e.Graphics.DrawString("GRAND TOTAL : " & TotalPrice, Font10Bold, Brushes.Black, RightMargin, 15 + Height2, Right)
+        e.Graphics.DrawString("THANK YOU ", Font8, Brushes.Black, CentreMargin, 50 + Height2, centre)
+
+    End Sub
+    Dim TotalPrice As Long
+    Sub Sumprice()
+        Dim CountPrice As Long = 0
+        For RowItem As Long = 0 To dgvList.RowCount - 1
+            CountPrice = CountPrice + Val(dgvList.Rows(RowItem).Cells(2).Value)
+
+        Next
+        TotalPrice = CountPrice
+    End Sub
+
+    Private Sub btnPrint_Click(sender As Object, e As EventArgs) Handles btnPrint.Click
+        PPD.Document = PD
+        PPD.ShowDialog()
+
+    End Sub
+End Class
